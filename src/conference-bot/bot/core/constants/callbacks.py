15a55108f1f8f9c9e1a6ccffebca9a325e2959c1
NazipovaL <laleart2015@gmail.com>
2026-04-07 19:29:43 +0400

    1. Исправлена ошибка в порядке добавления конференции, генерации поста и промежуточных проверок 2. Подкорректированы сообщения-заглушки

--- src/conference-bot/bot/core/constants/callbacks.py
+++ src/conference-bot/bot/core/constants/callbacks.py
@@ -7,6 +7,8 @@ ADD_TAG = "add_tag"
 SAVE = "save"
 CANCEL_ADD_ADMIN = "cancel_add_admin"
 CONFIRM_ADD_ADMIN = "confirm_add_admin"
+CONFIRM_SAVE_CONF = "confirm_save_conf"
+CANCEL_SAVE_CONF = "cancel_save_conf"
 
 # User callbacks
 MAIN_MENU = "main_menu"
--- src/conference-bot/bot/core/handlers/admin/add_conference.py
+++ src/conference-bot/bot/core/handlers/admin/add_conference.py
@@ -3,8 +3,8 @@ from aiogram.types import CallbackQuery, Message
 from aiogram.fsm.context import FSMContext
 
 from bot.core.states.states import AddConference
-from bot.core.keyboards import back_button, ocr_buttons, data_buttons, hashtags_keyboard
-from bot.core.constants import MESSAGES
+from bot.core.keyboards import back_button, ocr_buttons, data_buttons, hashtags_keyboard, post_edit_buttons, generate_post_buttons, confirm_conf_buttons
+from bot.core.constants import MESSAGES, callbacks as cb
 from bot.core.callbacks import AdminCallback, FlowCallback
 from bot.services import AdminService,ConferenceService
 
@@ -56,13 +56,61 @@ async def data_ok(callback: CallbackQuery, state: FSMContext):
     await callback.answer()
     tags = ConferenceService.get_default_tags()
     await state.update_data(available_tags=tags, selected_tags=[])
-    await state.set_state(AddConference.hashtags)
+    # await state.set_state(AddConference.hashtags)
+    # await callback.message.edit_text(
+    #     MESSAGES["add-conf"]["add-tags"],
+    #     reply_markup=hashtags_keyboard(tags, [])
+    # )
+    await state.set_state(AddConference.confirm_save)
     await callback.message.edit_text(
-        MESSAGES["add-conf"]["add-tags"],
-        reply_markup=hashtags_keyboard(tags, [])
+        MESSAGES["add-conf"]["confirm-save"],
+        reply_markup = confirm_conf_buttons()
     )
 
+# @router.callback_query(F.data == cb.CONFIRM_SAVE_CONF)
+# async def confirm_save_conf(callback: CallbackQuery, state: FSMContext):
+#     await callback.answer()
     
+#     data = await state.get_data()
+    
+#     # TODO: проверка на дубликаты
+#     # TODO: сохранение в БД
+    
+#     await callback.message.edit_text(
+#         MESSAGES["add-conf"]["saved-success"],
+#         reply_markup=generate_post_buttons()
+#     )
+    
+    
+@router.callback_query(F.data == cb.CONFIRM_SAVE_CONF)
+async def confirm_save_conf(callback: CallbackQuery, state: FSMContext):
+    await callback.answer()
+
+    data = await state.get_data()
+
+    # сохранить в БД
+
+    await callback.message.edit_text(
+        MESSAGES["add-conf"]["saved-success"],
+        reply_markup=generate_post_buttons()
+    )    
+    
+    
+@router.callback_query(FlowCallback.filter(F.action == "generate_post"))
+async def generate_post(callback: CallbackQuery, state: FSMContext):
+    await callback.answer()
+    
+    data = await state.get_data()
+    post_text = ConferenceService.build_post(data)
+    
+    await state.update_data(post_text=post_text)
+    await state.set_state(AddConference.post)
+    
+    await callback.message.edit_text(
+        post_text,
+        reply_markup=post_edit_buttons(),
+        parse_mode="HTML"
+    )   
     
 @router.callback_query(FlowCallback.filter(F.action == "ocr_edit"))
 async def ocr_edit(callback: CallbackQuery):
@@ -73,4 +121,17 @@ async def ocr_edit(callback: CallbackQuery):
         "Пока что просто нажмите 'Всё верно' для продолжения тестирования.",
         reply_markup=ocr_buttons(),
         parse_mode="Markdown"
+    )
+
+@router.callback_query(FlowCallback.filter(F.action == "to_tags"))
+async def to_tags(callback: CallbackQuery, state: FSMContext):
+    await callback.answer()
+    
+    tags = ConferenceService.get_default_tags()
+    await state.update_data(available_tags=tags, selected_tags=[])
+    await state.set_state(AddConference.hashtags)
+    
+    await callback.message.edit_text(
+        MESSAGES["add-conf"]["add-tags"],
+        reply_markup=hashtags_keyboard(tags, [])
     )
